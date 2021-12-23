8cc56b09d04d6d98bffc951427a3b587fd728809
Victor Vattier <victor.vattier35@gmail.com>
2021-12-24 00:59:58 +0100

    + animation chevron fix

--- script/filtersdeployed.js
+++ script/filtersdeployed.js
@@ -1,12 +1,10 @@
 const filters = document.querySelectorAll(".filter-container")
 const arrowDown = document.querySelector(".fa-chevron-down")
 
-filters.forEach(filter => filter.addEventListener("click", (e) => {
+filters.forEach((filter) => filter.addEventListener("click", () => {
     if (!filter.classList.contains("deployed")) {
         filter.classList.add("deployed")
-        arrowDown.style.transform = "rotate(180deg)"
     } else {
         filter.classList.remove("deployed")
-        arrowDown.style.transform = "rotate(0deg)"
     }
 }))
